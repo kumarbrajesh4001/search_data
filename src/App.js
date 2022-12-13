d65e3441ad26fe79b32449ed141ec4f6d737adf9
Brajesh Kumar <kumarbrajesh4001@gmail.com>
2022-12-13 22:40:20 +0530

    search text in previos data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const [data1, setData1] = useState([]);
+
+  const [defaultShowData, setDefaultShowData] = useState(true);
+  // console.log(data);
+
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  // const allMatchData = [];
+
+  const searchData = () => {
+    
+    const inputValue = document.getElementById("int").value;
+    for (let i = 0; i < data.length; i++) {
+
+      // if (data[i].title.includes(inputValue)) {
+      //   allMatchData.push(data[i]);
+      // }
+
+      
+      data[i].title.includes(inputValue) && setData1([data[i]]);
+      
+
+     
+    }
+
+    setDefaultShowData(false);
+  };
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search Box</h1>
+      <input type="text" id="int" />
+      <button
+        onClick={() => {
+          searchData();
+        }}
+      >
+        Search
+      </button>
+      {defaultShowData
+        ? data.map((cv) => (
+            <div key={cv.id}>
+              <div>{cv.title}</div>
+              <div>{cv.body}</div>
+            </div>
+          ))
+        : data1.map((cv) => (
+            <div key={cv.id}>
+              <div>{cv.title}</div>
+              <div>{cv.body}</div>
+            </div>
+          ))}
     </div>
   );
 }
